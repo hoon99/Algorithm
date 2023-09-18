9628512b26820d5beae510bb582113450aaf2fde
Jaehoon <79782580+hoon99@users.noreply.github.com>
2023-09-19 00:41:38 +0900

    공 바꾸기.py

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,8 +1,8 @@
 N, M = map(int, input().split())
-a = []
-for i in range(1, N+1):
-  a.append(i)
+a = [i for i in range(1, N+1)]
+
 for i in range(M):
   x, y = map(int, input().split())
   a[x-1], a[y-1] = a[y-1], a[x-1]
+  
 print(*a)
