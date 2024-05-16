cda88fb6cc5b022863b70b3a0e865966927ed80a
Jaehoon <79782580+hoon99@users.noreply.github.com>
2024-05-16 14:30:24 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,18 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    
+    for i in reserve[:]:
+        if i in lost:
+            reserve.remove(i)
+            lost.remove(i)
+    
+    for i in lost[:]:
+        if i-1 in reserve:
+            reserve.remove(i-1)
+            lost.remove(i)
+        elif i+1 in reserve:
+            reserve.remove(i+1)
+            lost.remove(i)
+    
+    return n - len(lost)
