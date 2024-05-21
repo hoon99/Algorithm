bc01cf2d181ad59d48738ecba4d1b4cb21ff61f8
Jaehoon <79782580+hoon99@users.noreply.github.com>
2024-05-21 17:54:59 +0900

    [level 1] Title: 덧칠하기, Time: 391.40 ms, Memory: 12.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,12 @@
+def solution(n, m, section):
+    count = 1
+    left = section[0]
+    
+    for i in section[:]:
+        if i < left + m:
+            section.remove(i)
+        else:
+            left = i
+            count += 1
+        
+    return count
