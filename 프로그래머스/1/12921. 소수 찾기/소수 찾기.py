ac4de3f45443938109286050f253abc1fc068346
Jaehoon <79782580+hoon99@users.noreply.github.com>
2024-05-21 16:54:29 +0900

    [level 1] Title: 소수 찾기, Time: 3204.62 ms, Memory: 93.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    count = 0
+    nums = set([i for i in range(2, n+1)])
+    
+    for i in range(2, n+1):
+        for j in range(2, int(i**0.5)+1):
+            if i % j == 0:
+                nums.remove(i)
+                break
+    
+    return len(nums)
